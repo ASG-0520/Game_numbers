27bd4b6056bbb799920b72489398613a0875e83e
Aleks <usdru@mail.ru>
2020-02-09 20:35:26 +0100

    Merge branch 'master' of C:\Git\Game_numbers with conflicts.

--- game_numbers.py
+++ game_numbers.py
@@ -1,26 +1,27 @@
 import os
 import time
 import random
-from colorama import init
+from colorama import init  #https://pypi.org/project/colorama/
 from colorama import Fore, Back, Style
 
 init()
 while True:
     os.system('cls')
-    print("\n    Запомни последовательность чисел за 3 секунд,\n"
+    print("\n    Запомни последовательность чисел за 3 секунды,\n"
           " после введи их через пробел.\n")
+    input("Press <ENTER> key to START GAME...")
+    os.system('cls')
     sp = []
     sp_inp = []
     while sp == list(sp_inp):
         sp.append(random.randint(0, 9))
         print(sp)
-        time.sleep(3)  # полностью останавливает программу на 3 сек
+        input("Press <ENTER> to CONTINUE...")
         os.system('cls')
         sp_inp = map(int, input(' ').split())
     # os.system('cls')
     print(Fore.CYAN + Style.NORMAL + '\nТы ошибся!\n'
-                                     'Правильно: ' + Style.RESET_ALL + str(
-        sp) + '\n' + Fore.GREEN + Style.BRIGHT + "\nТвой LVL = " + str(len(sp) - 1))
+                                     'Правильно: ' + Style.RESET_ALL + str(sp) + '\n' + Fore.GREEN + Style.BRIGHT + "\nТвой LVL = " + str(len(sp) - 1))
     print(Fore.RED + Style.BRIGHT + "\n### GAME OVER! ###" + Style.RESET_ALL)
     # a = input()
     if input('\nПовторить?(y/n)') == 'n':
