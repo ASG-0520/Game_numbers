698da865c1b319ff08bd122f0e7c0eee687370a2
Aleks <usdru@mail.ru>
2020-02-09 23:32:32 +0100

    Ver 2.1 pip-8

--- game_numbers.py
+++ game_numbers.py
@@ -1,15 +1,15 @@
 import os
 import time
 import random
-from colorama import init  #https://pypi.org/project/colorama/
+from colorama import init  # https://pypi.org/project/colorama/
 from colorama import Fore, Back, Style
 
 init()
+print(Fore.CYAN + Style.DIM + "\n    Запомни последовательность чисел за 3 секунды,\n"
+                              " как экран очистится введи их через пробел.\n" + Style.RESET_ALL)
+input(" Для начала игры нажмите <ENTER>")
+
 while True:
-    os.system('cls')
-    print("\n    Запомни последовательность чисел за 3 секунды,\n"
-          " после введи их через пробел.\n")
-    input("Для начала игры нажминте <ENTER>")
     os.system('cls')
     sp = []
     sp_inp = []
@@ -19,10 +19,9 @@ while True:
         time.sleep(3)
         os.system('cls')
         sp_inp = map(int, input(' ').split())
-    # os.system('cls')
     print(Fore.CYAN + Style.NORMAL + '\nТы ошибся!\n'
-                                     'Правильно: ' + Style.RESET_ALL + str(sp) + '\n' + Fore.GREEN + Style.BRIGHT + "\nТвой LVL = " + str(len(sp) - 1))
+                                     'Правильно: ' + Style.RESET_ALL + str(sp) +
+          '\n' + Fore.GREEN + Style.BRIGHT + "\nТвой LVL = " + str(len(sp) - 1))
     print(Fore.RED + Style.BRIGHT + "\n### GAME OVER! ###" + Style.RESET_ALL)
-    # a = input()
     if input('\nПовторить?(y/n)') == 'n':
         break
